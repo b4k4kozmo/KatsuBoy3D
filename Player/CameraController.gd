d98744ebfd984cf95a313cfe1b070761f05c2306
Cosmo Steiner-Schlede <106993529+b4k4kozmo@users.noreply.github.com>
2023-08-16 12:22:40 -0700

    Pause added
    pause screen added that stops movement of player and camera

--- Player/CameraController.gd
+++ Player/CameraController.gd
@@ -12,28 +12,29 @@ func _process(delta):
 	global_position = $"..".global_position
 
 func _input(event):
-	if event is InputEventMouse:
-		# xRot is clamped so we dont put the camera under the floor 
-		var xRot = clamp(rotation.x - event.relative.y / 1000 * sensitivity, -.5, 0.25)
-		# yRot is not clamped so we can rotate the camera 360 degrees
-		var yRot = rotation.y - event.relative.x / 1000 * sensitivity
-		# we do not want to rotate along the Z axis so we set x to 0
-		rotation = Vector3(xRot, yRot, 0)
-		#this is only needed to display mouse motion in the debug menu
-		#print_debug(event)
-		
-	if event is InputEventMouseButton:
-		if event.button_index == 5:
-			if $SpringArm3D.spring_length < maxZoom:
-				$SpringArm3D.spring_length += 0.1
-		if event.button_index == 4:
-			if $SpringArm3D.spring_length > minZoom:
-				$SpringArm3D.spring_length -= 0.1
+	if not $"..".playerPaused:
+		if event is InputEventMouse:
+			# xRot is clamped so we dont put the camera under the floor 
+			var xRot = clamp(rotation.x - event.relative.y / 1000 * sensitivity, -.5, 0.25)
+			# yRot is not clamped so we can rotate the camera 360 degrees
+			var yRot = rotation.y - event.relative.x / 1000 * sensitivity
+			# we do not want to rotate along the Z axis so we set x to 0
+			rotation = Vector3(xRot, yRot, 0)
+			#this is only needed to display mouse motion in the debug menu
+			#print_debug(event)
 			
-	# test input for track pad usint - and =
-	if Input.is_action_pressed("zoom_out"):
-		if $SpringArm3D.spring_length < maxZoom:
-			$SpringArm3D.spring_length += 0.5
-	if Input.is_action_pressed("zoom_in"):
-		if $SpringArm3D.spring_length > minZoom:
-			$SpringArm3D.spring_length -= 0.5
+		if event is InputEventMouseButton:
+			if event.button_index == 5:
+				if $SpringArm3D.spring_length < maxZoom:
+					$SpringArm3D.spring_length += 0.1
+			if event.button_index == 4:
+				if $SpringArm3D.spring_length > minZoom:
+					$SpringArm3D.spring_length -= 0.1
+				
+		# test input for track pad usint - and =
+		if Input.is_action_pressed("zoom_out"):
+			if $SpringArm3D.spring_length < maxZoom:
+				$SpringArm3D.spring_length += 0.5
+		if Input.is_action_pressed("zoom_in"):
+			if $SpringArm3D.spring_length > minZoom:
+				$SpringArm3D.spring_length -= 0.5
--- Player/Player.gd
+++ Player/Player.gd
@@ -4,49 +4,59 @@ extends CharacterBody3D
 var run_speed = speed * 2
 @export var jump_impulse = 33
 @export var fall_acceleration = 50
+var playerPaused: bool = false
 
 var target_velocity = Vector3.ZERO
 
 
 func _physics_process(delta):
-	# create a locakl variable to store input direction
-	var direction = Vector3.ZERO
-	# get camera direction
-	var hRot =$CameraController.transform.basis.get_euler().y
-	# check for each move input and update the direction accordingly
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	
-	# x and z are the ground plane in 3D
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+	if not playerPaused:
+		# create a locakl variable to store input direction
+		var direction = Vector3.ZERO
+		# get camera direction
+		var hRot =$CameraController.transform.basis.get_euler().y
+		# check for each move input and update the direction accordingly
+		if Input.is_action_pressed("move_right"):
+			direction.x += 1
+		if Input.is_action_pressed("move_left"):
+			direction.x -= 1
 		
-	if direction != Vector3.ZERO:
-		# this line of code gets diretion relative to the camera direction
-		direction = direction.rotated(Vector3.UP, hRot).normalized()
-		$Pivot.look_at(position + direction, Vector3.UP)
+		# x and z are the ground plane in 3D
+		if Input.is_action_pressed("move_back"):
+			direction.z += 1
+		if Input.is_action_pressed("move_forward"):
+			direction.z -= 1
+			
+		if direction != Vector3.ZERO:
+			# this line of code gets diretion relative to the camera direction
+			direction = direction.rotated(Vector3.UP, hRot).normalized()
+			$Pivot.look_at(position + direction, Vector3.UP)
+			
 		
-	
-	
-	# Ground velocity
-	if Input.is_action_pressed("run"):
-		target_velocity.x = direction.x * run_speed
-		target_velocity.z = direction.z * run_speed
-	else:
-		target_velocity.x = direction.x * speed
-		target_velocity.z = direction.z * speed
-	# Vertical velocity
-	if not is_on_floor(): # if in the air fall to the floor (gravity)
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-	
-	# moving the character
-	velocity = target_velocity
-	move_and_slide()
-	
-	# jumping
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		target_velocity.y = jump_impulse
+		
+		# Ground velocity
+		if Input.is_action_pressed("run"):
+			target_velocity.x = direction.x * run_speed
+			target_velocity.z = direction.z * run_speed
+		else:
+			target_velocity.x = direction.x * speed
+			target_velocity.z = direction.z * speed
+		# Vertical velocity
+		if not is_on_floor(): # if in the air fall to the floor (gravity)
+			target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+		
+		# moving the character
+		velocity = target_velocity
+		move_and_slide()
+		
+		# jumping
+		if is_on_floor() and Input.is_action_just_pressed("jump"):
+			target_velocity.y = jump_impulse
+
+
+func _on_canvas_layer_game_paused():
+	playerPaused = true
+
+
+func _on_canvas_layer_game_not_paused():
+	playerPaused = false
