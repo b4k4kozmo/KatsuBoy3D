f05e6898c4f4f8b0acd78492c34c038a4d169813
Cosmo Steiner-Schlede <106993529+b4k4kozmo@users.noreply.github.com>
2023-09-18 15:54:06 -0700

    added features
    can now stack items, each item has a max stack amount. numbers only show when you are holding more than 1 item in a slot

--- KamiAxe.gd
+++ KamiAxe.gd
@@ -1,8 +1,10 @@
 extends "res://Collectable.gd"
 
 @onready var animation = $MeshInstance3D/woodsmansaxe/AnimationPlayer
+@onready var collision = $CollisionShape3D
 
 func collect(inventory: Inventory):
+	collision.queue_free()
 	animation.play("spin")
 	await animation.animation_finished
 	super(inventory)
